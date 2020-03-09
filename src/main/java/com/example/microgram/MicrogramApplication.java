73c87ff30843b24221dced6f01785ea54bfe7c71
abdygany_tabyldyev <tabyldyev34@gmail.com>
2020-03-09 15:15:40 +0600

    Main класс для запуска программы

---
+++ src/main/java/com/example/microgram/MicrogramApplication.java
@@ -0,0 +1,16 @@
+package com.example.microgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrogramApplication {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO HELLO");
+        SpringApplication.run(MicrogramApplication.class, args);
+    }
+
+
+
+}
